3690a00ba2fdd6b087b989451637c691420160c5
Luis David Fernández <luis.david.fer@gmail.com>
2020-04-26 00:28:11 +0200

    Templates includes

---
+++ template.js
@@ -0,0 +1,17 @@
+// Map properties
+let map = L.map("map", {
+    minZoom: 2,
+    maxZoom: {{arg}},
+    zoomControl: false
+});
+// Controls properties
+L.control.zoom({position:"bottomright"}).addTo(map);
+// Image properties
+let img = new L.RasterCoords(map, [{{arg}},{{arg}}]);
+// Tiles properties
+L.tileLayer('tiles/{z}/{x}/{y}.png', {
+    noWrap: true,
+    attribution: '<a title="Leaflet plugin for plain image map projection to display large images using tiles generated with gdal2tiles-leaflet" href="https://github.com/commenthol/leaflet-rastercoords">Leaflet-rastercoords</a>' + ' | ' + '<a title="Generate raster image tiles for use with Leaflet" href="https://github.com/commenthol/gdal2tiles-leaflet">Gdal2tiles-leaflet</a>' + ' | ' + '<a title="Python 3 utility to generate Leaflet based interactives maps from plain images." href="https://www.github.com/luisdavidfer/image2map">Image2map</a>'
+}).addTo(map);
+// Map initial view
+map.setView(img.unproject([img.width/2, img.height/2]), 3);
